9c4a1c6576257129bcfe41eb05b5eb25f5b83793
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-06-28 22:06:15 -0700

    Edit HTTP requests to query MongoDB

--- index.js
+++ index.js
@@ -1,84 +1,26 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Models = require('./models');
 
 const app = express();
 
-const movies = [
-  {
-    title: 'Point Break',
-    description:
-      'An F.B.I. Agent goes undercover to catch a gang of surfers who may be bank robbers.',
-    genre: {
-      name: 'Action',
-      description:
-        'Action films are built around a core set of characteristics: spectacular physical action; a narrative emphasis on fights, chases, and explosions; and a combination of state-of-the-art special effects and stunt-work.',
-    },
-    director: {
-      name: 'Kathryn Bigelow',
-      bio: 'Kathryn Ann Bigelow is an American filmmaker. Covering a wide range of genres, her films include Near Dark, Point Break, Strange Days, K-19: The Widowmaker, The Hurt Locker, Zero Dark Thirty, and Detroit.',
-      birth: '1951',
-      death: 'N/A',
-    },
-    Released: '1991',
-    imageUrl:
-      'https://www.imdb.com/title/tt0102685/mediaviewer/rm1454182656/?ref_=tt_ov_i,',
-    featured: 'true',
-  },
-  {
-    title: 'Tombstone',
-    description: `A successful lawman's plans to retire anonymously in Tombstone, Arizona are disrupted by the kind of outlaws he was famous for eliminating.`,
-    genre: {
-      name: 'Drama',
-      description: `The drama genre features stories with high stakes and many conflicts. They're plot-driven and demand that every character and scene move the story forward.`,
-    },
-    director: {
-      name: 'George P. Cosmatos',
-      bio: 'George Pan Cosmatos was a Greek-Italian film director and screenwriter. Following early success in his home country with drama films such as Massacre in Rome.',
-      birth: '1941',
-      death: '2005',
-    },
-    Released: '1993',
-    imageUrl:
-      'https://www.imdb.com/title/tt0108358/mediaviewer/rm2107331584/?ref_=tt_ov_i',
-    featured: false,
-  },
-  {
-    title: 'Snatch',
-    description:
-      'Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.',
-    genre: {
-      name: 'Comedy',
-      description:
-        'Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter, especially in theatre, film, stand-up comedy, television, radio, books, or any other entertainment medium. ',
-    },
-    director: {
-      name: 'Guy Ritchie',
-      bio: 'Guy Stuart Ritchie is an English film director, producer and screenwriter. His work includes British gangster films, and the Sherlock Holmes films starring Robert Downey Jr. Ritchie left school at age 15 and worked entry-level jobs in the film industry before going on to direct television commercials.',
-      birth: '1968',
-      death: 'N/A',
-    },
-    Released: '2000',
-    imageUrl:
-      'https://www.imdb.com/title/tt0208092/mediaviewer/rm1248859904/?ref_=tt_ov_i',
-    featured: true,
-  },
-];
+app.use(bodyParser.json());
 
-const users = [
-  {
-    username: 'MovieLover123',
-    password: 'Password',
-    email: 'movielover123@gmail.com',
-    topMovies: ['Point Break'],
-  },
-];
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(morgan('common'));
 
-app.use(bodyParser.json());
-
 app.use(express.static('public'));
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://127.0.0.1:27017/cfDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.get('/', (req, res) => {
   res.send('Pardon our dust, movie database is under construction');
@@ -86,108 +28,131 @@ app.get('/', (req, res) => {
 
 // Gets the list of data about all movies
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
+  Movies.find()
+    .then((movies) => {
+      res.status(201).json(movies);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error ${err}`);
+    });
 });
 
 // Gets the data about a single movie by title
 app.get('/movies/:title', (req, res) => {
-  const foundMovie = movies.find(
-    (movie) => movie.title.toLowerCase() === req.params.title.toLowerCase()
-  );
+  const title = new RegExp(`^${req.params.title}$`, 'i');
 
-  if (!foundMovie) {
-    res
-      .status(404)
-      .send(`Movie with the title ${req.params.title} was not found`);
-  } else {
-    res.status(200).json(foundMovie);
-  }
+  Movies.findOne({Title: {$regex: title}})
+    .then((movie) => {
+      if (movie) {
+        res.status(200).json(movie);
+        return;
+      }
+      res.status(404).send('No movie found with that title');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Server error: ${err}`);
+    });
 });
+
 // return a list of movies by genre
 app.get('/movies/genres/:genreName/movies', (req, res) => {
-  const moviesByGenre = movies.filter(
-    (movie) =>
-      movie.genre &&
-      movie.genre.name.toLowerCase() === req.params.genreName.toLowerCase()
-  );
+  const {genreName} = req.params;
+  const regex = new RegExp(`^${genreName}$`, 'i'); // case-insensitive search
 
-  if (moviesByGenre.length === 0) {
-    res
-      .status(404)
-      .send(`No movies found with the genre ${req.params.genreName}`);
-  } else {
-    res.status(200).json(moviesByGenre);
-  }
+  Movies.find({'Genre.Name': {$regex: regex}})
+    .select('Title')
+    .then((movieTitles) => {
+      if (movieTitles.length === 0) {
+        return res.status(404).send(`Genre ${genreName} was not found`);
+      }
+      res.status(200).json(movieTitles);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
-// Get detial about a genre by name
+// Get detail about a genre by name
 app.get('/movies/genres/:genreName', (req, res) => {
-  const moviesByGenre = movies.filter(
-    (movie) =>
-      movie.genre &&
-      movie.genre.name.toLowerCase() === req.params.genreName.toLowerCase()
-  );
+  const {genreName} = req.params;
+  const genreQuery = new RegExp(`^${genreName}$`, 'i'); // case-insensitive search
 
-  if (moviesByGenre.length === 0) {
-    res.status(404).send(`No genre found for genre ${req.params.genreName}`);
-  } else {
-    // create a new array with only the genre name and description
-    const genres = moviesByGenre.map((movie) => ({
-      name: movie.genre.name,
-      description: movie.genre.description,
-    }));
-
-    // remove duplicate genre entries
-    const uniqueGenres = [
-      ...new Set(genres.map((genre) => JSON.stringify(genre))),
-    ].map((genre) => JSON.parse(genre));
-    res.status(200).json(uniqueGenres);
-  }
+  Movies.findOne({'Genre.Name': {$regex: genreQuery}})
+    .select('Genre')
+    .then((genre) => {
+      if (!genre) {
+        return res.status(404).send(`Genre ${genreName} was not found`);
+      }
+      res.status(200).json(genre);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
 // Get all details about director by name
 app.get('/movies/directors/:directorName', (req, res) => {
-  const moviesByDirector = movies.filter(
-    (movie) =>
-      movie.director &&
-      movie.director.name.toLowerCase() ===
-        req.params.directorName.toLowerCase()
-  );
+  const {directorName} = req.params;
+  const directorQuery = new RegExp(`^${directorName}$`, 'i'); // case insensitive search
 
-  if (moviesByDirector.length === 0) {
-    res.status(404).send(`Director ${req.params.directorName} was not found`);
-  } else {
-    // create a new array with only the genre name and description
-    const directors = moviesByDirector.map((movie) => ({
-      name: movie.director.name,
-      bio: movie.director.bio,
-      birth: movie.director.birth,
-      death: movie.director.death,
-    }));
-    // remove any duplicate entries
-    const uniqueDirectors = [
-      ...new Set(directors.map((director) => JSON.stringify(director))),
-    ].map((director) => JSON.parse(director));
-    res.status(200).json(uniqueDirectors);
-  }
+  Movies.findOne({'Director.Name': {$regex: directorQuery}})
+    .select('Director')
+    .then((director) => {
+      if (!director) {
+        return res.status(404).send(`Director ${directorName} was not found`);
+      }
+      res.status(200).json(director);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error ${err}`);
+    });
+});
+
+// Return a list of movies by a certain director
+
+app.get('/movies/directors/:directorName/movies', (req, res) => {
+  const {directorName} = req.params;
+  const regex = new RegExp(`^${directorName}$`, 'i'); // case-insensitive search
+
+  Movies.find({'Director.Name': {$regex: regex}})
+    .select('Title')
+    .then((movieTitles) => {
+      if (movieTitles.length === 0) {
+        return res.status(404).send(`No movies found by ${directorName}`);
+      }
+      res.status(200).json(movieTitles);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send(`Error: ${err}`);
+    });
 });
 
 // Allow new users to register
 app.post('/users', (req, res) => {
-  const newUser = req.body;
-  const userNameTaken = users.find(
-    (user) => user.username === newUser.username
-  );
+  Users.findOne({Username: req.body.Username}).then((user) => {
+    if (user) {
+      return res.status(400).send(`${req.body.Username} already exists`);
+    }
+    Users.create({
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Birthday: req.body.Birthday,
+    })
 
-  if (!newUser.username) {
-    res.status(400).send('Please enter username.');
-  } else if (userNameTaken) {
-    res.status(400).send('Username is taken.');
-  } else {
-    newUser.topMovies = [];
-    users.push(newUser);
-    res.status(201).json(newUser);
-  }
+      .then((user) => {
+        res.status(201).json(user).send(`${user} has been added sucessfully`);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send(`Error: ${err}`);
+      });
+  });
 });
 
 // Allow users to update username and information.
