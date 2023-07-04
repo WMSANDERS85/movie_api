1bdb86967456c3303d2678e5ec582328dee827a6
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-07-03 23:43:06 -0700

    Install Passport and configure Passport strategies

---
+++ auth.js
@@ -0,0 +1,35 @@
+const jwtSecret = 'your_jwt_secret'; // Name has to be the same key used on the JWTStrategy
+
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+require('./passport'); // local passport file
+
+const generateJWTToken = (user) => {
+  jwt.sign(user, jwtSecret, {
+    subject: user.Username, // username that is being encoded in the JWT
+    expiresIn: '7d', // token will expire after 7 days
+    algorithm: 'HS256', // algorithm used to 'sign' or encode the values of JWT
+  });
+};
+
+// POST login
+module.exports = (router) => {
+  router.post('/login', (req, res) => {
+    passport.authenticate('local', {session: false}, (error, user, info) => {
+      if (error || !user) {
+        return res.status(400).json({
+          message: 'Something is not right',
+          user,
+        });
+      }
+      req.login(user, {session: false}, (err) => {
+        if (err) {
+          res.send(err);
+        }
+        const token = generateJWTToken(user.toJSON());
+        return res.json({user, token});
+      });
+    })(req, res);
+  });
+};
---
+++ passport.js
@@ -0,0 +1,48 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const passportJWT = require('passport-jwt');
+const Models = require('./models');
+
+const Users = Models.User;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'Username',
+      passwordField: 'Password',
+    },
+    (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      Users.findOne({Username: username}, (error, user) => {
+        if (error) {
+          console.log(error);
+          return callback(error);
+        }
+
+        if (!user) {
+          console.log('Incorrect username');
+          return callback(null, user);
+        }
+
+        console.log('finished');
+        return callback(null, user);
+      });
+    }
+  )
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+      secretOrKey: 'your_jwt_secret',
+    },
+    (jwtPayload, callback) =>
+      // eslint-disable-next-line no-underscore-dangle
+      Users.findById(jwtPayload._id)
+        .then((user) => callback(null, user))
+        .catch((error) => callback(error))
+  )
+);
