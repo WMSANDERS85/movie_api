3370bd2b3e97315c4ce34b563d1f59414c93df3f
Wayne Sanders <107742337+WMSANDERS85@users.noreply.github.com>
2023-07-07 14:53:25 -0700

    Change mongoose conncection to promise

--- index.js
+++ index.js
@@ -58,18 +58,16 @@ auth(app);
 app.use(express.static('public'));
 
 // Database connection
-try {
-  mongoose.connect(
-    process.env.MONGODB_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    },
-    () => console.log('Connected to the database')
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to the database'))
+  .catch((error) =>
+    console.log('Database connection failed. Error message: ', error.message)
   );
-} catch (error) {
-  console.log('Database connection failed. Error message: ', error.message);
-}
 
 app.get('/', (req, res) => {
   res.send('Pardon our dust, movie database is under construction');
